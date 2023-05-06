e96dc067f69539c55fd33cd1b7b4d2956b941106
JiAlbano <jialbano@gbox.adnu.edu.ph>
2023-05-06 16:36:10 +0800

    edit admindb css design.

--- resources/views/admindb.blade.php
+++ resources/views/admindb.blade.php
@@ -4,41 +4,40 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Appointment Overview</title>
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito">
     <style>
+       @import url('https://fonts.googleapis.com/css2?family=Concert+One&display=swap');
         body {
             background: url("../assets/bg2.png") no-repeat center center fixed !important;
             background-size: cover !important;
-            font-family: "Montserrat", sans-serif;
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
         }
 
         table {
-            border-collapse: collapse;
-            margin-top: 50px;
-            width: 80%;
-            background-color: #fff;
-            border: 2px solid #000;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-        }
+  border-collapse: collapse;
+  margin-top: 5px;
+  width: 100%;
+  text-align: center;
+  background-color: #fff;
+  border: 2px solid #000;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
 
-        table th {
-            padding: 10px;
-            background-color: #333;
-            color: #fff;
-            border: 1px solid #000;
-            font-size: 18px;
-            font-weight: bold;
-            text-transform: uppercase;
-        }
+td {
+  width: 20%;
+  padding: 10px;
+  text-align: center;
+}
+
+
+table th {
+  padding: 10px;
+  background-color: #333;
+  color: #fff;
+  border: 1px solid #000;
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
 
         table td {
             padding: 10px;
@@ -55,68 +54,44 @@
             background-color: #ddd;
         }
 
-        h1 {
-    	text-align: center;
-    	font-family: "Nunito", sans-serif; /* change font family to Nunito */
-   	 	font-size: 36px;
-    	margin-top: 30px;
-    	font-weight: bold;
-			}
-
-        p {
-            font-size: 24px;
-            text-align: center;
-            margin-top: 50px;
-            font-weight: bold;
-            text-transform: uppercase;
-        }
-        .form-control {
-            padding: 10px;
-            margin: 0px 0px 20px 0px;
-            font-size: 18px;
-            border: 1px solid #5F85B2;
-            border-radius: 5px;
-        }
-
-        .form-control:focus {
-            outline: none;
-            box-shadow: none;
-            border: 1px solid #5F85B2;
-        }
-
-        .form-check-input {
-            margin-top: 5px;
-        }
-
-        .btn-primary {
-            background-color: #5F85B2;
-            border: none;
-            border-radius: 5px;
-            padding: 10px 20px;
-            font-size: 18px;
-            font-weight: bold;
-            text-transform: uppercase;
-            margin-right: 10px;
-            cursor: pointer;
-        }
-
-        .btn-primary:hover {
-            background-color: #34495E;
-        }
-
-        .btn-link{
-color: #3498DB;
-text-decoration: none;
+        .tdesign {
+    background-color: transparent !important;
+    font-family: "Concert One", cursive !important;
+    color: #1F87E8 !important;
+    font-weight: 900 !important;
+    text-align: center !important;
+    border: none !important;
+    padding: 10px 10px !important;
+    text-transform: uppercase;
 }
-  .btn-link:hover {
-        color: #2980B9;
-    }
+        .buff {
+  padding: 0px 0px 0px 5px!important;
+  max-height: 400px;
+  overflow: auto;
+  margin: 0 auto!important;
+  text-align: center; /* added to center the table */
+}
+
+.buff2 {
+  
+  padding: 40px 0px 0px 5px!important;
+  max-width: 400px!important;
+  margin: 0 auto!important;
+}
+
 	</style>
 </head>
 
 @section('content')
+<div class="buff2 col-md-8 offset-md-10">
+<img src="{{ asset('assets/car2.png') }}" alt="Car Image" width="70" height="70" style="display:block; margin:auto;">
+    <div class= "tdesign mb-2">
     <h1>Appointment Overview</h1>
-    <div class="container">
+    <hr style="border-top: 5px solid #000080">
+    </div>
+</div>
+    
+    <div class="buff col-md-8 offset-md-10">
         <table>
             <thead>
                 <tr>
@@ -128,13 +103,15 @@ text-decoration: none;
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>{{ $name }}</td>
-                    <td>{{ $email }}</td>
-                    <td>{{ $contact_number }}</td>
-                    <td>{{ $service_type }}</td>
-                    <td>{{ $datetime }}</td>
-                </tr>
+                @foreach ($appointments as $appointment)
+                    <tr>
+                        <td>{{ $appointment->name }}</td>
+                        <td>{{ $appointment->email }}</td>
+                        <td>{{ $appointment->contact_number }}</td>
+                        <td>{{ $appointment->service_type }}</td>
+                        <td>{{ $appointment->datetime }}</td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
